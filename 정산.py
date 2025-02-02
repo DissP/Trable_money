3870b934642d6c921c7402b1449fbcbe457ac7f4
DissP <kwakhw3020@naver.com>
2025-02-02 23:54:51 +0900

    1차 정산

--- 정산.py
+++ 정산.py
@@ -61,73 +61,10 @@
     "Agoda 예약번호2"  : "1520908728" ,
     "숙박기간" : "2025-03-05 ~ 2025-03-06",
 
+    "이름" : "karaksa hotel Sapporo" ,
+    "주소" : "5-24 Minami 3-jo Nishi, Chuo-ku, Sapporo-shi, Hokkaido, 삿포로, 일본, 060-0063",
+    "Agoda 예약번호1"  : "1520998864" ,
+    "숙박기간" : "2025-03-06 ~ 2025-03-09",
+
 }
-
-
-
-
-# [삿포로역 주변 인기 호텔]
-# 삿포로 그랜드 호텔
-# http://www.hotelscombined.co.kr/Hotel/Sapporo_Grand_Hotel.htm?a_aid=199910
-#
-# 크로스 호텔 삿포로
-# http://www.hotelscombined.co.kr/Hotel/Cross_Hotel_Sapporo.htm?a_aid=199910
-#
-# 호텔 몬토레 삿포로
-# http://www.hotelscombined.co.kr/Hotel/Hotel_Monterey_Sapporo.htm?a_aid=199910
-#
-# JR 인 삿포로 에키 미나미 구치
-# http://www.hotelscombined.co.kr/Hotel/JR_INN_Sapporo_eki_Minami_guchi.htm?a_aid=199910
-#
-#
-#
-#
-# [오도리공원 & 스스키노 역 근처 인기 호텔]
-# 삿포로 뷰 호텔 우도리 코우엔
-# http://www.hotelscombined.co.kr/Hotel/Sapporo_View_Hotel_Oodori_Kouen.htm?a_aid=199910
-#
-# 호텔 레솔 트리니티 삿포로
-# http://www.hotelscombined.co.kr/Hotel/Hotel_Resol_Trinity_Sapporo.htm?a_aid=199910
-#
-# 삿포로 도큐 레이 호텔
-# http://www.hotelscombined.co.kr/Hotel/Sapporo_Tokyu_REI_Hotel.htm?a_aid=199910
-#
-#
-#
-#
-# [나카지마코엔역 근처 인기 호텔]
-# 삿포로 엑셀 호텔 도큐
-# http://www.hotelscombined.co.kr/Hotel/Sapporo_Excel_Hotel_Tokyu.htm?a_aid=199910
-#
-# 삿포로 파크 호텔
-# http://www.hotelscombined.co.kr/Hotel/Sapporo_Park_Hotel.htm?a_aid=199910
-#
-# 이비스 스타일스 삿포로
-# http://www.hotelscombined.co.kr/Hotel/Ibis_Styles_Sapporo.htm?a_aid=199910
-#
-#
-#
-#
-# [삿포로 온천 호텔]
-# 도미인 삿포로 아넥스
-# http://www.hotelscombined.co.kr/Hotel/Dormy_Inn_Sapporo_Annex.htm?a_aid=199910
-#
-# 도미인 프리미엄 삿포로
-# http://www.hotelscombined.co.kr/Hotel/Dormy_Inn_PREMIUM_Sapporo.htm?a_aid=199910
-#
-#
-#
-#
-# [삿포로 호스텔 & 게스트하우스]
-# 그리드 삿포로 호텔&호스텔
-# http://www.hotelscombined.co.kr/Hotel/Grids_Sapporo_HotelHostel.htm?a_aid=199910
-#
-
-# 오타루나이 백패커스 호스텔 모리노키
-# https://blog.naver.com/jrhhere/220912764055
-
-# 오타루 탭 룸 앤 호스텔
-# https://www.agoda.com/ko-kr/otaru-tap-room-craft-beer-hostel/hotel/otaru-jp.html?countryId=3&finalPriceView=1&isShowMobileAppPrice=false&cid=1922868&numberOfBedrooms=&familyMode=false&adults=1&children=0&rooms=1&maxRooms=0&isCalendarCallout=false&childAges=&numberOfGuest=0&missingChildAges=false&travellerType=0&showReviewSubmissionEntry=false&currencyCode=KRW&isFreeOccSearch=false&tag=666e69b4-446d-49dd-909e-e9614aee8412&tspTypes=9%2C16%2C8%2C-1&los=1&searchrequestid=c44b308c-7afd-4e0c-a033-8116af53e5d2&ds=F%2BX4hxYYB8zj5tBF&checkin=2025-03-05
-
-
 
